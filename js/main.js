7e6677ac7bf3ae7a1235a56ff55cd77c551bfe7e
Anurag Phukan <reach.anuragph@gmail.com>
2021-01-28 10:13:43 +0530

    Add user input feature

--- js/main.js
+++ js/main.js
@@ -1,36 +1,49 @@
-const rows = 16;
-const columns = 16;
-const numberOfCells = rows * columns;
+const defaultSize = 16
 
-const canvas = document.querySelector('.canvas');
+const erase = document.querySelector('.erase');
+erase.addEventListener('click', eraseCanvas);
 
-// Create cells and add them to canvas.
-for(let i = 1; i <= numberOfCells; i++ ) {
-    const cell = document.createElement('div');
-    cell.setAttribute('id', i);
-    cell.classList.add('cell');
-    canvas.appendChild(cell);
+createCanvas(defaultSize);
+
+
+function createCanvas(cellsPerSide) {
+    const numberOfCells = cellsPerSide ** 2;
+
+    const canvas = document.querySelector('.canvas');
+    // Clear canvas (required after erase).
+    canvas.innerHTML = '';
+
+    // Create cells with event listeners.
+    for(let i = 1; i <= numberOfCells; i++ ) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener('mouseover', () => draw(cell))
+
+        canvas.appendChild(cell);
+    }
+
+    // Display cells in grid.
+    canvas.style['display'] = 'grid';
+    canvas.style['grid-template-columns'] = `repeat(${cellsPerSide}, auto)`;
 }
 
-// Display cells in grid.
-canvas.style['display'] = 'grid';
-canvas.style['grid-template-columns'] = `repeat(${columns}, auto)`;
-canvas.style['grid-template-rows'] = `repeat(${rows}, auto)`;
 
 // Draw feature.
-const getCell = document.querySelectorAll('.cell');
-
-getCell.forEach(item => {item.addEventListener('mouseover', () => draw(item))});
-
 function draw(onCell) {
+
     onCell.classList.add('active');
 }
 
+
 // Erase feature.
-const erase = document.querySelector('.erase');
-
-erase.addEventListener('click', eraseCanvas);
-
 function eraseCanvas() {
-    getCell.forEach(item => item.classList.remove('active'));
+
+    // User input.
+    let newGrid = prompt('Choose size for new canvas');
+
+    if (newGrid <= 0 || newGrid > 100) {
+        newGrid = prompt('Please choose between 1 and 100')
+    }
+
+    createCanvas(newGrid);
 }
